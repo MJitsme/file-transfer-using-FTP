9cf32d32bfa83f0d9abc8a536b58b2257eff59bb
Milan Varghese Joseph <125987303+MJitsme@users.noreply.github.com>
2023-08-07 15:24:21 +0530

    Update client.c

--- client.c
+++ client.c
@@ -13,3 +13,39 @@
 #define SAI struct sockaddr_in
 
 //function to send file
+void send_file(int sockfd)
+{
+  char buff[MAX]={0};
+  fp=fopen("send.txt","r");
+  while(fgets(data,MAX,fp)!=NULL)
+  {
+    send(sockfd,buff,sizeof(buff),0);
+    bzero(buff,MAX);
+  }
+}
+
+//main function
+int main()
+{
+  int sockfd;
+  SAI server;
+  sockfd=socket(AF_INET,SOCK_STREAM,0);
+  printf("socket created successfully\n");
+  server.sin_family=AF_INET;
+  server.sin_addr.s_addr=htonl(INADDR_ANY);
+  server.sin_port=htons(PORT);
+
+  if(connect(sockfd,(SA*)&server,sizeof(server))==0);
+  {
+    printf("Connection successful..\n");
+  }
+  else
+  {
+    printf("Connection failed..");
+    return 1;
+  }
+
+  send_file(sockfd);
+  close(sockfd);
+  return 0;
+}
